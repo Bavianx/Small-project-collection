870004143866e72b3e7ca7f6b7bb727e3c0b14e1
Mochi x <87911875+Bavianx@users.noreply.github.com>
2026-01-01 20:33:45 +0000

    Update atm_machine.py

--- ATM_machine/atm_machine.py
+++ ATM_machine/atm_machine.py
@@ -13,21 +13,33 @@ def atm_machine():
         try:
             user = int(input("Please select your choice (1-4): "))
         except ValueError:
-            print("Please input the correct parameters!")
+            print("Invalid number. Please enter the correct one from the list!")
             continue
             
         if user == 1:
-            deposit = int(input("Please input the amount you would like to deposit: $ "))
-            balance += deposit
-            print(f"Your balance is now: $", {balance} )
+            try:
+                deposit = int(input("Please input the amount you would like to deposit: $ "))
+            except ValueError:
+                print("Invalid amount. Please enter a number")
+                question = input("Are you sure you would like to follow through with this deposit? (y/n): ").lower()
+                if question == 'y':
+                    balance += deposit
+                    print(f"Your balance is now: $", {balance} )
+                else:
+                    continue
 
         elif user == 2:
-            withdraw = int(input("Please request how much money you would like to withdraw: $ "))
-            if withdraw > balance:
-                print("You have insufficient funds!")
-            else:
-                balance -= withdraw
-                print(f"You have withdrawn:", {withdraw} ,"Your balance is now: $ ", {balance})
+             try:
+                withdraw = int(input("Please request how much money you would like to withdraw: $ "))
+             except ValueError:
+                print("Invalid amount. Please enter a number")
+                question = input("Are you sure you would like to follow through with this withdrawl? (y/n): ").lower()
+                if question == 'y':
+                    if withdraw > balance:
+                        print("You have insufficient funds!")
+                    else:
+                        balance -= withdraw
+                        print(f"You have withdrawn:", {withdraw} ,"Your balance is now: $ ", {balance})
 
         elif user == 3:
             print(f"Your balance is: $", {balance})
