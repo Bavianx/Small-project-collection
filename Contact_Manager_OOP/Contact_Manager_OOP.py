60b2e0aac28709d7623bc2080951b4979982e5be
Mochi x <87911875+Bavianx@users.noreply.github.com>
2026-04-08 15:54:48 +0100

    Implement JSON persistence and improve input handling
    Added JSON data persistence for contacts and implemented load_storage method to load contacts from a JSON file. Updated user input handling and ensured data is saved after adding or removing contacts.

--- Contact_Manager_OOP/Contact_Manager_OOP.py
+++ Contact_Manager_OOP/Contact_Manager_OOP.py
@@ -1,5 +1,3 @@
-#Json data persistance needs to be added and then the application is almost done
-
 import json, os
 class Contact: 
     def __init__(self, name, email, mobile):  
@@ -34,10 +32,42 @@ class ContactBook:
             print(f"Save failed: {e}")
             return False
         
+    def load_storage(self, filename):
+        try:
+            with open(filename, "r") as f:
+                data = json.load(f)
+
+            for contact_data in data:
+                contact = Contact(
+                    contact_data["name"],
+                    contact_data["email"],
+                    contact_data["mobile"]
+                )
+                self.contacts[contact.name] = contact
+
+            with open(filename + ".backup", 'w') as f:
+                json.dump(data, f, indent=4)
+            print(f"Loaded data from {filename}")
+
+        except FileNotFoundError:
+            print("Contact list not found, Starting fresh!")
+            return {}
+        except json.JSONDecodeError:
+            print(f"Corruption of your {filename} file")
+            backup = filename + ".corrupted"
+            print(f"Saving corrupted file as {backup}")   
+
+            try:
+                os.rename(filename, backup)             
+            except:
+                pass                                       
+            print("Starting with a new contact list.")
+            return {}  
+        
     def add_contact(self, contact):
         self.contacts[contact.name] = contact    #OBJECT[individual contacts name] = contact variable
     
-    def view_contact(self, name):
+    def view_contact(self, name):       #o(1) lookup as we are getting the users data directly from the dictionary key "name" 
         if name in self.contacts:           #searches if the individual input is within the contacts dictionary
             contact = self.contacts[name]   #if it is then it will get the contacts name and display this as contact for call back 
             print(f"Name: {name} | Email: {contact.email} | Mobile: {contact.mobile}")  #name is displayed from input and the contact call back is now used to leverage the object .notation
@@ -52,17 +82,19 @@ class ContactBook:
         print("Current Contacts:")
         for name, contact in self.contacts.items():     #gets all of the data for the names within the contacts dictionary
             print(f"Name: {name} | Email: {contact.email} | Mobile: {contact.mobile}")  #prints the data with all of the contacts as we are getting the items
-            print("="*40)
+        print("="*40)
 
-    def search_email(self, email):      #o(n)lookup (searching for email within a name loop as the email isnt the key)  # functionality is useless but learning code efficiency
+    def search_email(self, email):           #o(n)lookup (searching for email within a name loop as the email isnt the key)  # functionality is useless but learning code efficiency
         for name, contact in self.contacts.items():
             if contact.email == email:
                 print(f"Email: {contact.email} | Name: {name} | Mobile: {contact.mobile}")
+                print("="*40)
                 return
-            print("="*40)
         print(f"{email} could not be found within your contacts")
     
+
 contacts_list = ContactBook("My Contacts")
+contacts_list.load_storage("Contact.json")
 while True:
     print("================== Contact Manager ==================")
     print("1.) Add Contact")
@@ -80,36 +112,64 @@ while True:
         print("==============================")
     
     if choice == 1:
-        name = input("Enter Contacts Name: ")            #Error handling needs to be added across the choices
-        print("==============================")        
+        name = input("Enter Contacts Name or 'B' to return: ").upper()
+        if name == 'B':
+            continue
+        if not name.strip():
+            print("This field cannot be left empty!")
+            print("============================================")
+            continue
         email = input("Please input the Contacts email: ")
-        print("==============================")
+        if not email.strip():
+            print("This field cannot be left empty!")
+            print("============================================")
+            continue
         mobile = input("Please input the Contacts Mobile number: ")
-        print("==============================")
+        if not mobile.strip():
+            print("This field cannot be left empty!")
+            print("============================================")
+            continue
+        
         new_contact = Contact(name, email, mobile)
         contacts_list.add_contact(new_contact)
+        contacts_list.save_storage("Contact.json")
     elif choice == 2:
         name = input("Enter the contacts name: ")
+        if name == 'B':
+            continue
+        if not name.strip():
+            print("This field cannot be left empty!")
+            print("============================================")
+            continue
         contacts_list.view_contact(name)
     elif choice == 3:
         contacts_list.view_all_contacts()
     elif choice == 4:
         name = input("Please input the Contacts name you would like removing: ")
+        if name == 'B':
+            continue
+        if not name.strip():
+            print("This field cannot be left empty!")
+            print("============================================")
+            continue
         confirm = input(f"Are you sure you would like to remove {name}?: ").lower()
         if confirm == "y": 
             del contacts_list.contacts[name]
-            #implement JSON if save and load is added here
+            contacts_list.save_storage("Contact.json")
             print(f"Successfully removed {name} from your contacts!")
         else:
             print("Operation has been cancelled!")
             continue
     elif choice == 5:
         email = input("Please input the email to search: ")
+        if email == 'B':
+            continue
         contacts_list.search_email(email)
 
     elif choice == 6:
         Exit = input("Are you sure you would like to leave the application?: ").lower()
         if Exit == "y":
+            contacts_list.save_storage("Contact.json")
             print("Thank you for using your contact book!")
             break
         else:
