7cb3d64b4301cb014ba61f98d7a9767c88e04d11
Mochi x <87911875+Bavianx@users.noreply.github.com>
2026-04-22 15:25:04 +0100

    Delete Django_Applications directory

--- Django_Applications/mysite/books/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from books import views
-
-urlpatterns = [
-    path('', views.book_list, name='book_list'),
-    path('<int:pk>/', views.book_detail, name='book_detail'), #Captures whatever integer is in the URL and passes it into the view as pk (books/1/ , book/2/, book/3/  etc) achieved through leveraging the primary key (PK) 
-]
